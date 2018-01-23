7c47366e7f89e20e0256519eae0bc080266b646a
Sabrina Rubert <sabrinarubert@sebastismacbook.fritz.box>
2018-01-23 21:45:23 +0100

    bug gefixt

--- KittyCat/ViewController.swift
+++ KittyCat/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: UIViewController {
     
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+
+        super.viewDidAppear(animated)
         changeColor()
         startHeartAnimation()
         startBlinkingEyes ()
